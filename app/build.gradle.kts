b65f3aacae754e448fcc3ed97b332c3fc9bdcd04
Felix Brucker <felix@felixbrucker.com>
2026-09-19 18:24:19 +1200

    Prepare release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "com.felixbrucker.simklcalendar"
     minSdk = 26
     targetSdk = 37
-    versionCode = 1
-    versionName = "1.0.0"
+    versionCode = 2
+    versionName = "1.1.0"
     buildConfigField("String", "APP_NAME", "\"simkl-calendar\"")
   }
 
